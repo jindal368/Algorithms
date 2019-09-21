3f50aa4dd7cb2aa524e03b8029df304896a2e6e0
Vishesh jindal <46999893+jindal368@users.noreply.github.com>
2019-09-21 10:37:33 +0530

    Add files via upload

---
+++ Cut_the_sticks.c
@@ -0,0 +1,158 @@
+#include <assert.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+char** split_string(char*);
+
+// Complete the cutTheSticks function below.
+
+// Please store the size of the integer array to be returned in result_count pointer. For example,
+// int a[3] = {1, 2, 3};
+//
+// *result_count = 3;
+//
+// return a;
+//
+int* cutTheSticks(int arr_count, int* arr, int* result_count) {
+int i,min,j,c=0,k=0,m,t=arr_count,p;
+static int r[1000];
+
+for(i=0;i<arr_count;i++)
+{
+  for(j=i+1;j<arr_count;j++)
+  {
+    if(arr[i]>arr[j])
+    {
+      p=arr[i];
+      arr[i]=arr[j];
+      arr[j]=p;
+    }
+  }
+}
+while(arr_count>0)
+{  c=0;
+   min=arr[0];
+ 
+  for(j=0;j<arr_count;j++){
+    arr[j]=arr[j]-min;
+    c++;
+    
+  }
+  r[k]=c;
+  k++;
+      while(arr[0]==0){
+    
+      for(m=1;m<arr_count;m++)
+       arr[m-1]=arr[m];
+       arr_count--;
+      if(arr_count==0)
+       break;
+    
+  
+  }*result_count=k;
+}
+return r;
+}
+
+int main()
+{
+    FILE* fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+    char* n_endptr;
+    char* n_str = readline();
+    int n = strtol(n_str, &n_endptr, 10);
+
+    if (n_endptr == n_str || *n_endptr != '\0') { exit(EXIT_FAILURE); }
+
+    char** arr_temp = split_string(readline());
+
+    int* arr = malloc(n * sizeof(int));
+
+    for (int i = 0; i < n; i++) {
+        char* arr_item_endptr;
+        char* arr_item_str = *(arr_temp + i);
+        int arr_item = strtol(arr_item_str, &arr_item_endptr, 10);
+
+        if (arr_item_endptr == arr_item_str || *arr_item_endptr != '\0') { exit(EXIT_FAILURE); }
+
+        *(arr + i) = arr_item;
+    }
+
+    int arr_count = n;
+
+    int result_count;
+    int* result = cutTheSticks(arr_count, arr, &result_count);
+
+    for (int i = 0; i < result_count; i++) {
+        fprintf(fptr, "%d", *(result + i));
+
+        if (i != result_count - 1) {
+            fprintf(fptr, "\n");
+        }
+    }
+
+    fprintf(fptr, "\n");
+
+    fclose(fptr);
+
+    return 0;
+}
+
+char* readline() {
+    size_t alloc_length = 1024;
+    size_t data_length = 0;
+    char* data = malloc(alloc_length);
+
+    while (true) {
+        char* cursor = data + data_length;
+        char* line = fgets(cursor, alloc_length - data_length, stdin);
+
+        if (!line) { break; }
+
+        data_length += strlen(cursor);
+
+        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') { break; }
+
+        size_t new_length = alloc_length << 1;
+        data = realloc(data, new_length);
+
+        if (!data) { break; }
+
+        alloc_length = new_length;
+    }
+
+    if (data[data_length - 1] == '\n') {
+        data[data_length - 1] = '\0';
+    }
+
+    data = realloc(data, data_length);
+
+    return data;
+}
+
+char** split_string(char* str) {
+    char** splits = NULL;
+    char* token = strtok(str, " ");
+
+    int spaces = 0;
+
+    while (token) {
+        splits = realloc(splits, sizeof(char*) * ++spaces);
+        if (!splits) {
+            return splits;
+        }
+
+        splits[spaces - 1] = token;
+
+        token = strtok(NULL, " ");
+    }
+
+    return splits;
+}
